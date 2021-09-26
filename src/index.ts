329e833afe5dbcf04a24a93e445f40971c498f83
Aiklyukov <muzo0@yandex.ru>
2021-09-26 12:01:46 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { Task } from "@lather/core";
+import { setPower } from "./commands/setPower";
+import { setColor } from "./commands/setColor";
+import { ledDiscovery } from "./ledDiscovery";
+import { reqParser } from "./reqParser";
+
+const app = ledDiscovery.chain(([control]) =>
+  reqParser.chain(({ req, res, params }) =>
+    Task.structPar({
+      power: setPower(params.power),
+      color: setColor(params.color, params.brightness, params.power),
+    })
+      .provide({ control })
+      .tap(() => {
+        res.writeHead(200);
+        res.end("ok");
+      })
+  )
+);
+
+app
+  .provide({
+    port: 8923,
+    host: "0.0.0.0",
+    scanLimit: 500,
+  })
+  .run();
